e3cc5baadc9da48e57b9a5de61bae50c665f5d85
BrazhnikDA <65236303+BrazhnikDA@users.noreply.github.com>
2021-11-24 02:35:01 +0300

    Main function for launching the project

---
+++ src/main/kotlin/com/harman/viewtable/app.kt
@@ -0,0 +1,13 @@
+package com.harman.viewtable
+
+/**
+ * The main function for launching the project
+ */
+fun main() {
+    ViewTable("Join Horizontal")
+    ViewTable("Join Vertical")
+    ViewTable("Join Chart Horizontal")
+    ViewTable("JoIn ChArT VERTICAL")
+    ViewTable("Hello world")
+}
+
